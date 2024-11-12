70d029d006b251d34d3cf034056d3b9af4b8a5b5
Sohelrana2815 <sohel152302@gmail.com>
2024-11-12 14:40:16 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
+import { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 1000);
+
+    // Cleanup function
+
+    if (count >= 10) {
+      clearInterval(interval);
+      console.log("Interval cleared at 10 seconds");
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]); // Only run once
+
+  return <h1>Timer: {count}</h1>;
 };
 
 export default App;
