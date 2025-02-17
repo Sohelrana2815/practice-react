7a74da9d4f4419ef0ff64331116e5d4c0ac97625
Sohelrana2815 <sohel152302@gmail.com>
2025-02-17 07:58:34 +0600

    done

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,50 +1,9 @@
-import { useState, useEffect } from "react";
-import PostsList from "./PostsList";
 
 const Home = () => {
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch(
-          "https://jsonplaceholder.typicode.com/users"
-        );
-        const data = await response.json();
-
-        setPosts(data);
-      } catch (error) {
-        console.error("Error fetching posts:", error);
-      }
-    };
-    fetchPosts();
-  }, []);
-  console.log(posts);
-
   return (
-    <>
-      <div>
-        <h2>{posts.length}</h2>
-        <div className="overflow-x-auto">
-          <table className="table">
-            {/* head */}
-            <thead>
-              <tr>
-                <th></th>
-                <th>Name</th>
-                <th>Job</th>
-                <th>Favorite Color</th>
-              </tr>
-            </thead>
-            <tbody>
-              {posts.map((post) => (
-                <PostsList key={post.id} post={post} />
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </>
+    <div>
+      
+    </div>
   );
 };
 
